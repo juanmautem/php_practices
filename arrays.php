3cc4595272651b8afda4961a2d4e51398c88abc0
juanmautem <manuel-fernandez@utem.edu.mx>
2022-09-27 10:14:19 -0500

    ejemplo de array

---
+++ arrays.php
@@ -0,0 +1,25 @@
+<?php
+
+	$vector = [1,2,3];
+
+	$array = array(
+	    "foo" => "bar",
+	    "bar" => "foo",
+	);
+
+
+	$array2 = [
+	    "foo" => "bar",
+	    "bar" => "foo",
+	];
+
+	echo "<h3>Arreglo PHP < 5.0 </h3>";
+	print_r($array);
+	echo "<h3>Arreglo PHP > 5.1 </h3>";
+	print_r($array2);
+
+	// rellenar una matriz 4x4 (aleatorio / manual); 
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,11 +1,32 @@
+<?php 
+	$i = 10;
+	$cadena = "Anita lava la tina";
+	$fl = 12.45;
+	$vector = [0,2,4];
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
+	<title>Prácticas PHP</title>
 </head>
 <body>
+	<center><h1>Prácticas PHP <hr></h1></center>
+	<?php 
+		echo "<button>El valor de I = " . $i . ";</button>";
+		echo "<p>El valor de cadena = " . $cadena . ";</p>";
+		echo "<p>El valor de fl = " . $fl . ";</p>";
+		echo "<p>El valor de vector = "; 
+		print_r($vector);
+		echo ";</p>";
 
+	?>
+	<hr>
+	<?php
+	require_once "arrays.php";
+	?>
 </body>
 </html>
